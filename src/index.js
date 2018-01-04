90dd14a7d57ad79f1c2041faed61146bd85b20dc
siwalikm <siwalikm@gmail.com>
2018-01-04 19:45:13 +0530

    …

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-import { app, BrowserWindow } from 'electron';
+import { Menu, app, shell, BrowserWindow, dialog } from 'electron';
+const defaultMenu = require('electron-default-menu');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
+
+app.on('ready', () => {
+
+  // Get template for default menu
+  const menu = defaultMenu(app, shell);
+
+  // Add custom menu
+  menu.splice(4, 0, {
+    label: 'Custom',
+    submenu: [
+      {
+        label: 'Do something',
+        click: (item, focusedWindow) => {
+          dialog.showMessageBox({message: 'Do something', buttons: ['OK'] })
+        }
+      }
+    ]
+  })
+
+  // Set top-level application menu, using modified template
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menu));
+})
+
 
 const createWindow = () => {
   // Create the browser window.
